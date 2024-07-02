2d213575fff545603927ec29f253ae0802fc2728
Peal Hasan <pealh0320@gmail.com>
2024-07-01 21:31:17 -0400

    Bug fixed

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -8,209 +8,119 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var body_parser_1 = require("body-parser");
-var cors_1 = require("cors");
-var db_1 = require("../database/db");
-var userService_1 = require("../database/services/userService");
-var userService_2 = require("../database/services/userService");
-var todoService_1 = require("../database/services/todoService");
-var todoService_2 = require("../database/services/todoService");
-var todoService_3 = require("../database/services/todoService");
-var app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const db_1 = __importDefault(require("../database/db"));
+const userService_1 = require("../database/services/userService");
+const userService_2 = require("../database/services/userService");
+const todoService_1 = require("../database/services/todoService");
+const todoService_2 = require("../database/services/todoService");
+const todoService_3 = require("../database/services/todoService");
+const app = (0, express_1.default)();
+app.use(
+  (0, cors_1.default)({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // Allow cookies to be sent with the requests
-}));
+  })
+);
 app.use(express_1.default.json());
-var port = 3001;
+const port = 3001;
 app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.get("/", function () {
+app.get("/", () => {
     console.log("server running");
 });
-app.post("/signup", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var email, password, user;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                console.log("Post requested on signup");
-                return [4 /*yield*/, db_1.default];
-            case 1:
-                _a.sent();
-                email = req.body.email;
-                password = req.body.password;
-                return [4 /*yield*/, (0, userService_2.findUser)(email)];
-            case 2:
-                user = _a.sent();
-                if (!user) return [3 /*break*/, 3];
-                res.status(200).json({ status: 0, message: "User already exists" });
-                return [3 /*break*/, 5];
-            case 3: return [4 /*yield*/, (0, userService_1.createUser)(email, password)];
-            case 4:
-                _a.sent();
-                res.status(200).json({ status: 1, message: "Sign up successful" });
-                _a.label = 5;
-            case 5: return [2 /*return*/];
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Post requested on signup");
+    yield db_1.default;
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = yield (0, userService_2.findUser)(email);
+    if (user) {
+        res.status(200).json({ status: 0, message: "User already exists" });
+    }
+    else {
+        yield (0, userService_1.createUser)(email, password);
+        res.status(200).json({ status: 1, message: "Sign up successful" });
+    }
+}));
+app.post("/add", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield db_1.default;
+    console.log("Add reaquest made");
+    try {
+        const email = req.body.email;
+        const text = req.body.text;
+        const user = yield (0, userService_2.findUser)(email);
+        if (user) {
+            console.log("PPP " + user._id + " " + text);
+            const todo = yield (0, todoService_1.createTodo)(user._id, text);
+            console.log("Successfully added todo" + todo);
+            res.status(200).json({ status: 1, message: "To do added" });
         }
-    });
-}); });
-app.post("/add", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var email, text, user, todo, _a;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0: return [4 /*yield*/, db_1.default];
-            case 1:
-                _b.sent();
-                console.log("Add reaquest made");
-                _b.label = 2;
-            case 2:
-                _b.trys.push([2, 6, , 7]);
-                email = req.body.email;
-                text = req.body.text;
-                return [4 /*yield*/, (0, userService_2.findUser)(email)];
-            case 3:
-                user = _b.sent();
-                if (!user) return [3 /*break*/, 5];
-                console.log("PPP " + user._id + " " + text);
-                return [4 /*yield*/, (0, todoService_1.createTodo)(user._id, text)];
-            case 4:
-                todo = _b.sent();
-                console.log("Successfully added todo" + todo);
-                res.status(200).json({ status: 1, message: "To do added" });
-                _b.label = 5;
-            case 5: return [3 /*break*/, 7];
-            case 6:
-                _a = _b.sent();
-                console.log("Error adding task");
-                return [3 /*break*/, 7];
-            case 7: return [2 /*return*/];
+    }
+    catch (_a) {
+        console.log("Error adding task");
+    }
+}));
+app.post("/todos", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Post request of /todos");
+    yield db_1.default;
+    try {
+        const email = req.body.email;
+        const user = yield (0, userService_2.findUser)(email);
+        if (user) {
+            const todos = yield (0, todoService_2.retriveTodos)(user._id);
+            if (todos) {
+                const texts = todos.map((todo) => todo.text);
+                res.status(200).json({ status: 1, todos: texts });
+            }
         }
-    });
-}); });
-app.post("/todos", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var email, user, todos, texts, _a;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0:
-                console.log("Post request of /todos");
-                return [4 /*yield*/, db_1.default];
-            case 1:
-                _b.sent();
-                _b.label = 2;
-            case 2:
-                _b.trys.push([2, 6, , 7]);
-                email = req.body.email;
-                return [4 /*yield*/, (0, userService_2.findUser)(email)];
-            case 3:
-                user = _b.sent();
-                if (!user) return [3 /*break*/, 5];
-                return [4 /*yield*/, (0, todoService_2.retriveTodos)(user._id)];
-            case 4:
-                todos = _b.sent();
-                if (todos) {
-                    texts = todos.map(function (todo) { return todo.text; });
-                    res.status(200).json({ status: 1, todos: texts });
-                }
-                _b.label = 5;
-            case 5: return [3 /*break*/, 7];
-            case 6:
-                _a = _b.sent();
-                console.log("Error retriving todos");
-                return [3 /*break*/, 7];
-            case 7: return [2 /*return*/];
+    }
+    catch (_a) {
+        console.log("Error retriving todos");
+    }
+}));
+app.post("/delete", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Post request /delete made");
+    yield db_1.default;
+    try {
+        const email = req.body.email;
+        const note = req.body.note;
+        const user = yield (0, userService_2.findUser)(email);
+        if (user) {
+            const result = yield (0, todoService_3.deleteTodo)(user._id, note);
+            console.log("Todo deleted: " + result);
+            res.status(200).json({ status: 1 });
         }
-    });
-}); });
-app.post("/delete", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var email, note, user, result, _a;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0:
-                console.log("Post request /delete made");
-                return [4 /*yield*/, db_1.default];
-            case 1:
-                _b.sent();
-                _b.label = 2;
-            case 2:
-                _b.trys.push([2, 6, , 7]);
-                email = req.body.email;
-                note = req.body.note;
-                return [4 /*yield*/, (0, userService_2.findUser)(email)];
-            case 3:
-                user = _b.sent();
-                if (!user) return [3 /*break*/, 5];
-                return [4 /*yield*/, (0, todoService_3.deleteTodo)(user._id, note)];
-            case 4:
-                result = _b.sent();
-                console.log("Todo deleted: " + result);
-                res.status(200).json({ status: 1 });
-                _b.label = 5;
-            case 5: return [3 /*break*/, 7];
-            case 6:
-                _a = _b.sent();
-                console.log("Error retriving todos");
-                return [3 /*break*/, 7];
-            case 7: return [2 /*return*/];
+    }
+    catch (_a) {
+        console.log("Error retriving todos");
+    }
+}));
+app.post("/signin", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield db_1.default;
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = yield (0, userService_2.findUser)(email);
+    if (user) {
+        if (user.password === password) {
+            res.status(200).json({ status: 1, message: "Successful login" });
         }
-    });
-}); });
-app.post("/signin", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var email, password, user;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, db_1.default];
-            case 1:
-                _a.sent();
-                email = req.body.email;
-                password = req.body.password;
-                return [4 /*yield*/, (0, userService_2.findUser)(email)];
-            case 2:
-                user = _a.sent();
-                if (user) {
-                    if (user.password === password) {
-                        res.status(200).json({ status: 1, message: "Successful login" });
-                    }
-                    else {
-                        res.status(200).json({ status: 0, message: "Wrong password" });
-                    }
-                }
-                else {
-                    res.status(200).json({ status: -1, message: "Email not found" });
-                    console.log("Error in signin");
-                }
-                return [2 /*return*/];
+        else {
+            res.status(200).json({ status: 0, message: "Wrong password" });
         }
-    });
-}); });
-app.listen(port, function () {
-    console.log("Listening on port ".concat(port));
+    }
+    else {
+        res.status(200).json({ status: -1, message: "Email not found" });
+        console.log("Error in signin");
+    }
+}));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
+//# sourceMappingURL=index.js.map
