b2f1ddaa7c8e36103b9d6f1db64c0462c1fcc674
Peal Hasan <pealh0320@gmail.com>
2024-07-02 00:13:12 -0400

    bug fixed

--- server/dist/database/db.js
+++ server/dist/database/db.js
@@ -4,12 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-import env from "dotenv";
 
-env.config();
-
-const username = process.env.MONGO_USERNAME;
-const pass = process.env.MONGO_PASSWORD;
+const username = "pealh0320";
+const pass = "245810";
 const MONGO_URL = `mongodb+srv://${username}:${pass}@todo.uhmep5x.mongodb.net/?retryWrites=true&w=majority&appName=Todo`;
 mongoose_1.default.connect(MONGO_URL);
 const db = mongoose_1.default.connection;
