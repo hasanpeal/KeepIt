c60786d5595c9b7e6765e5f766c95e705452f791
Peal Hasan <pealh0320@gmail.com>
2024-07-01 22:32:24 -0400

    Buttons updated

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -11,6 +11,7 @@ function Todo() {
   const [items, setItems] = useState<string[]>([]);
   const [text, setText] = useState<string>("");
   const location = useLocation();
+  console.log(todos);
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -116,17 +117,15 @@ function Todo() {
               <form onSubmit={(event) => handleDelete(event, index)}>
                 <Button
                   shape="circle"
-                  color="error"
-                  size=""
+                  color="secondary"
+                  size="xs"
                   className="IndButton"
                   type="submit"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="20"
-                    height="20"
+                    width="45"
+                    height="60"
                     viewBox="0 0 50 50"
                   >
                     <path d="M25,2C12.319,2,2,12.319,2,25s10.319,23,23,23s23-10.319,23-23S37.681,2,25,2z M33.71,32.29c0.39,0.39,0.39,1.03,0,1.42	C33.51,33.9,33.26,34,33,34s-0.51-0.1-0.71-0.29L25,26.42l-7.29,7.29C17.51,33.9,17.26,34,17,34s-0.51-0.1-0.71-0.29	c-0.39-0.39-0.39-1.03,0-1.42L23.58,25l-7.29-7.29c-0.39-0.39-0.39-1.03,0-1.42c0.39-0.39,1.03-0.39,1.42,0L25,23.58l7.29-7.29	c0.39-0.39,1.03-0.39,1.42,0c0.39,0.39,0.39,1.03,0,1.42L26.42,25L33.71,32.29z"></path>
